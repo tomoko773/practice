262928fade3d0e58b40729de64792630091f4cec
tomoco <tomoco@localhost>
2025-07-03 00:02:46 +0900

    Java導入※必読　課題

---
+++ HelloWorld_OgiTomoko/src/sample/SelfIntroduction.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "荻　智子";
+		int age = 29;
+		String hobby = "映画鑑賞";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+
+	}
+
+}
